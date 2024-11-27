b181756f8cc94559b72adfde30ba4bb85231cd2d
KNishi03 <156240739+KNishi03@users.noreply.github.com>
2024-11-27 15:04:40 -0800

    fixed borrowingserviceapi

--- Lab5COS236/src/lab5/BorrowingServiceAPI.java
+++ Lab5COS236/src/lab5/BorrowingServiceAPI.java
@@ -1,30 +1,7 @@
 package lab5;
 
-public class BorrowingService implements BorrowingServiceAPI {
-
-    @Override
-    public boolean borrowBook(Member member, Book book) {
-        if (book != null && book.getIsAvailable() && !member.getBorrowedBooks().contains(book)) {
-            member.getBorrowedBooks().add(book);
-            book.setIsAvailable(false);
-            System.out.println("Borrowing book: " + book.getTitle());
-            return true;
-        } else {
-            System.out.println("Cannot borrow book: " + book.getTitle());
-            return false;
-        }
-    }
-
-    @Override
-    public boolean returnBook(Member member, Book book) {
-        if (book != null && member.getBorrowedBooks().contains(book)) {
-            member.getBorrowedBooks().remove(book);
-            book.setIsAvailable(true);
-            System.out.println("Returning book: " + book.getTitle());
-            return true;
-        } else {
-            System.out.println("Cannot return book: " + book.getTitle());
-            return false;
-        }
-    }
+public interface BorrowingServiceAPI {
+    boolean borrowBook(Member member, Book book);
+    boolean returnBook(Member member, Book book);
 }
+
