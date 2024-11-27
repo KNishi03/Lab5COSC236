7ba125b1451ee8460186f51b81b5aaa14d2f7ee0
KNishi03 <156240739+KNishi03@users.noreply.github.com>
2024-11-26 18:31:53 -0800

    Updated Ebook
    added functionality

--- Lab5COS236/src/lab5/Ebook.java
+++ Lab5COS236/src/lab5/Ebook.java
@@ -1,5 +1,36 @@
 package lab5;
 
-public class Ebook {
-
+public class EBook implements Book {
+    
+    private String title;
+    private boolean isAvailable;
+    
+    public EBook(String title) {
+        this.title = title;
+        this.isAvailable = true;
+    }
+    
+    @Override
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    @Override
+    public String toString() {
+        return "EBook: " + title;
+    }
+    
+    @Override
+    public void setIsAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+    
+    @Override
+    public boolean getIsAvailable() {
+        return isAvailable;
+    }
 }
