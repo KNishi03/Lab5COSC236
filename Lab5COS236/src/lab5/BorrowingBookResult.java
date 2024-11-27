fdcb863fa2948369f5209ccda9cf439c445b4f06
samdueck <sam.dueck@hotmail.com>
2024-11-27 14:55:58 -0800

    Update BorrowingBookResult code

--- Lab5COS236/src/lab5/BorrowingBookResult.java
+++ Lab5COS236/src/lab5/BorrowingBookResult.java
@@ -4,6 +4,18 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class BorrowingBookResult{
+	
+	private boolean isSuccess; private String borrowingMessage;
+	
+	// Constructor
+	public BorrowingBookResult(boolean isSuccess, String borrowingMessage) { 
+		this.isSuccess = isSuccess; 
+		this.borrowingMessage = borrowingMessage;
+	}
+	
+	// Getters and setters
+	// TODO implement getters and setters 
+	}
 
 
 }
