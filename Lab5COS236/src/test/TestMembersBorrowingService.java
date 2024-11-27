5747904a5c814a1ea47f3f58e0fbe87bbdd13a22
KNishi03 <156240739+KNishi03@users.noreply.github.com>
2024-11-27 14:54:03 -0800

    added test

---
+++ Lab5COS236/src/test/TestMembersBorrowingService.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import lab5.Member;
+import lab5.PaperBook;
+
+class TestMembersBorrowingService {
+
+
+}
